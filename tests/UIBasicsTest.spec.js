a69402855a12c0f29fc1ca47df256b9fdddbcadc
maau <maau@stibodx.com>
2024-12-30 00:33:52 +0600

    wait to get all content

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -3,6 +3,7 @@ const {test, expect} = require('@playwright/test');
 test('First Playwright test',async ({page})=> {
     const userName = page.locator("#username");
     const signIn = page.locator("#signInBtn");
+    const cardTitles = page.locator(".card-body a");
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     await userName.fill("rahulshetty");
     await page.locator("[type='password']").fill("learning");
@@ -12,5 +13,9 @@ test('First Playwright test',async ({page})=> {
     await userName.fill("");
     await userName.fill("rahulshettyacademy"); 
     await signIn.click();
-    console.log(await page.locator(".card-body a").nth(0).textContent());
+    //console.log(await cardTitles.nth(0).textContent());
+    //await page.waitForLoadState('networkidle');
+    await cardTitles.first().waitFor();
+    const allTitles = await cardTitles.allTextContents();
+    console.log(allTitles);
 });
