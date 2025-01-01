4769ea9e3f9f0707ddee4270e2eeea1b9f3d39f6
maau <maau@stibodx.com>
2025-01-01 23:57:30 +0600

    added user and role selection

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,9 +1,6 @@
 const {test, expect} = require('@playwright/test');
 
-test('First Playwright test',async ({page})=> {
-    const userName = page.locator("#username");
-    const signIn = page.locator("#signInBtn");
-    const cardTitles = page.locator(".card-body a");
+test('First Playwright test', async ({page})=> {
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     await userName.fill("rahulshetty");
     await page.locator("[type='password']").fill("learning");
@@ -15,6 +12,23 @@ test('First Playwright test',async ({page})=> {
     await signIn.click();
     //console.log(await cardTitles.nth(0).textContent());
     //await page.waitForLoadState('networkidle');
+    await cardTitles.first().waitFor();
+    const allTitles = await cardTitles.allTextContents();
+    console.log(allTitles);
+});
+
+test.only('UI Control', async ({page})=> {
+    const userName = page.locator("#username");
+    const signIn = page.locator("#signInBtn");
+    const cardTitles = page.locator(".card-body a");
+    const dropdown = page.locator("select.form-control");
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    await userName.fill("rahulshettyacademy");
+    await page.locator("[type='password']").fill("learning");
+    dropdown.selectOption("consult");
+    await page.locator('.radiotextsty').last().click();
+    await page.locator('#okayBtn').click();
+    await signIn.click();
     await cardTitles.first().waitFor();
     const allTitles = await cardTitles.allTextContents();
     console.log(allTitles);
