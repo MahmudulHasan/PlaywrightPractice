023613b3e326221f51777b2790d7f5a60947385d
maau <autul.aust@gmail.com>
2025-01-03 15:48:44 +0600

    added some more assertions

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -22,12 +22,17 @@ test.only('UI Control', async ({page})=> {
     const signIn = page.locator("#signInBtn");
     const cardTitles = page.locator(".card-body a");
     const dropdown = page.locator("select.form-control");
+    const documentlink = page.locator("[href*='documents-request']");
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     await userName.fill("rahulshettyacademy");
     await page.locator("[type='password']").fill("learning");
     dropdown.selectOption("consult");
     await page.locator('.radiotextsty').last().click();
+    expect(await page.locator('.radiotextsty').last()).toBeChecked();
     await page.locator('#okayBtn').click();
+    await page.locator("#terms").click();
+    expect(await page.locator('#terms')).toBeChecked();
+    await expect(documentlink).toHaveAttribute("class", "blinkingText");
     await signIn.click();
     await cardTitles.first().waitFor();
     const allTitles = await cardTitles.allTextContents();
