7d555b0282135973fbc54b74516bd581d9667aee
maau <autul.aust@gmail.com>
2025-01-06 23:15:13 +0600

    End to end validation using api and ui test

--- api-tests/webAPI.spec.js
+++ api-tests/webAPI.spec.js
@@ -1,7 +1,10 @@
 const {test, expect, request} = require('@playwright/test');
 const loginPayload = {userEmail:"anshika@gmail.com",userPassword:"Iamking@000"};
+const orderPayload = {orders: [{country: "Cuba", productOrderedId: "6581cade9fd99c85e8ee7ff5"}]};
 let token;
+let orderId;
 test.beforeAll( async () => {
+    //login api
      const apiContext = await request.newContext();
      const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
        { data:loginPayload });
@@ -9,61 +12,37 @@ test.beforeAll( async () => {
        const loginResponseJson = await loginResponse.json();
        token = loginResponseJson.token;
        console.log(token);
+    
+       //create order api
+      const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order", 
+        { data:orderPayload,
+            headers: {
+                'Authorization' : token,
+                'Content-Type': 'application/json'
+            }
+         });
+      const orderResponseJson = await orderResponse.json();
+      console.log(orderResponseJson);
+      orderId = orderResponseJson.orders[0];
+
 });
 
-test.beforeEach( ()=> {
-
-});
-
-test('Client App Login', async ({page})=> {
-    const userName = page.locator("#username");
-    const products = page.locator(".card-body");
-    const productCart = page.locator(".card-footer");
-    const signIn = page.locator("#signInBtn");
-    const cardTitles = page.locator(".card-body a");
-    const dropdown = page.locator("select.form-control"); 
-    const documentlink = page.locator("[href*='documents-request']");
-    const productName = "Zara Coat 4";
+test('Create order via api', async ({page})=> {
     await page.addInitScript(value => {
         window.localStorage.setItem('token', value);
     }, token);
     await page.goto("https://rahulshettyacademy.com/client/");
-    await cardTitles.first().waitFor();
-    const allTitles = await cardTitles.allTextContents();
-    console.log(allTitles);
-    const count = await products.count();
-    console.log(count);
-    for( let i = 0; i < count; i++) {
-        if(await products.nth(i).locator("a").textContent() === productName)  {
-            console.log(await products.nth(i).locator("a").textContent());
-            await productCart.nth(i).locator("button").click();
+    await page.locator("button[routerLink*='myorders']").click();
+    await page.locator("tbody").waitFor();
+    const rows = await page.locator("tbody tr");
+    for(let i = 0; i<await rows.count(); i++) {
+        const rowOrderId = await rows.nth(i).locator("th").textContent();
+        if(orderId.includes(rowOrderId)) {
+            await rows.nth(i).locator("button").first().click();
             break;
         }
     }
-    await page.locator("#navbarResponsive a").click();
-    await page.locator("table td a").first().waitFor();
-    const bool = await page.locator("h4:has-text('Samsung Note 8')").isVisible();
-    expect(bool).toBeTruthy();
-    //await page.pause();
-    await page.locator("button[class='btn btn-success']").click();
-    await page.locator("#country").pressSequentially("Ban");
-    const dropdownCountry = await page.locator(".container .suggestions");
-    await dropdownCountry.waitFor();
-    const optionCount = await dropdownCountry.locator("a").count();
-    for(let i = 0; i < optionCount; i++) {
-        const countryText = await dropdownCountry.locator("a").nth(i).textContent();
-        if(countryText === "Bangladesh") {
-            await dropdownCountry.locator("a").nth(i).click();
-            break;
-        }
-    }
-    await page.locator("footer div").click();
-    const checkbox = await page.locator("label[for='checkbox2']");
-    //await checkbox.waitFor();
-    //await page.locator("#checkbox2").waitFor().click();
-    await checkbox.click();
-    await page.locator("input[value='Purchase']").click();
-    const successMessage = await page.locator('[class="alert alert-success alert-dismissible"]').textContent();
-    await console.log(successMessage);
+    const orderIdDeatils = await page.locator(".col-text").textContent();
     await page.pause();
+    expect(orderId.includes(orderIdDeatils)).toBeTruthy();
 });
