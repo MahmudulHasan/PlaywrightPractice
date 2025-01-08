c9c71e27cba5f49ac34af753addce29a2cbbfb99
maau <autul.aust@gmail.com>
2025-01-09 00:17:09 +0600

    Implemented utils file for api test

--- api-tests/webAPI.spec.js
+++ api-tests/webAPI.spec.js
@@ -1,48 +1,30 @@
-const {test, expect, request} = require('@playwright/test');
-const loginPayload = {userEmail:"anshika@gmail.com",userPassword:"Iamking@000"};
-const orderPayload = {orders: [{country: "Cuba", productOrderedId: "6581cade9fd99c85e8ee7ff5"}]};
-let token;
-let orderId;
-test.beforeAll( async () => {
+const { test, expect, request } = require('@playwright/test');
+const { APiUtils } = require('./utils/APiUtils');
+const loginPayload = { userEmail: "anshika@gmail.com", userPassword: "Iamking@000" };
+const orderPayload = { orders: [{ country: "Cuba", productOrderedId: "6581cade9fd99c85e8ee7ff5" }] };
+let response;
+test.beforeAll(async () => {
     //login api
-     const apiContext = await request.newContext();
-     const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
-       { data:loginPayload });
-       expect(loginResponse.ok()).toBeTruthy();
-       const loginResponseJson = await loginResponse.json();
-       token = loginResponseJson.token;
-       console.log(token);
-    
-       //create order api
-      const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order", 
-        { data:orderPayload,
-            headers: {
-                'Authorization' : token,
-                'Content-Type': 'application/json'
-            }
-         });
-      const orderResponseJson = await orderResponse.json();
-      console.log(orderResponseJson);
-      orderId = orderResponseJson.orders[0];
-
+    const apiContext = await request.newContext();
+    const apiUtils = new APiUtils(apiContext, loginPayload);
+    response = await apiUtils.createOrder(orderPayload);
 });
 
-test('Create order via api', async ({page})=> {
+test('Create order via api', async ({ page }) => {
     await page.addInitScript(value => {
         window.localStorage.setItem('token', value);
-    }, token);
+    }, response.token);
     await page.goto("https://rahulshettyacademy.com/client/");
     await page.locator("button[routerLink*='myorders']").click();
     await page.locator("tbody").waitFor();
     const rows = await page.locator("tbody tr");
-    for(let i = 0; i<await rows.count(); i++) {
+    for (let i = 0; i < await rows.count(); i++) {
         const rowOrderId = await rows.nth(i).locator("th").textContent();
-        if(orderId.includes(rowOrderId)) {
+        if (response.orderId.includes(rowOrderId)) {
             await rows.nth(i).locator("button").first().click();
             break;
         }
     }
     const orderIdDeatils = await page.locator(".col-text").textContent();
-    await page.pause();
-    expect(orderId.includes(orderIdDeatils)).toBeTruthy();
+    expect(response.orderId.includes(orderIdDeatils)).toBeTruthy();
 });
